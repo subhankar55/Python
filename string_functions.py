f4550f9a7b45152e917c31d0ab1d6359ac390e36
Subhankar <maitisubhankar55@gmail.com>
2025-08-26 09:23:10 +0530

    string functions

---
+++ string_functions.py
@@ -0,0 +1,18 @@
+name = 'Subhankar'
+
+print(name.upper())
+
+print(name.lower())
+
+print(name.find('a'))
+
+print(name.replace("Subhankar","Ironman"))
+
+print('k' in name)
+
+print('z' in name)
+
+print(name)
+
+# All the above string function don't do any modification to the original string.
+# all they create a new string where they do all the modification 
